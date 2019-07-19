e3ea68280ca85350d3c47dbcf26171713e062902
CodeinHub <53020958+CodeinHub@users.noreply.github.com>
2019-07-18 21:22:58 -0400

    Update Hello.java

--- Demo/src/Hello.java
+++ Demo/src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("hi");
+		System.out.println("hi");
 		System.out.println("Hello Again");
 		System.out.println("Hello Again");
 	}
